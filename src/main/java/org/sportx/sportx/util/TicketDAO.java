a2c53a3351661668e2af3ea94983b16383e0c974
WaeiZ <147704513+WaeiZ@users.noreply.github.com>
2025-04-25 15:41:25 +0100

    Ticket
    Tickets a funcionar, fiz tambem uma pagina para depois de submeter o ticket aparecer que foi submetido com sucesso e com uma mensagem informativa.

--- src/main/java/org/sportx/sportx/util/TicketDAO.java
+++ src/main/java/org/sportx/sportx/util/TicketDAO.java
@@ -2,6 +2,7 @@ package org.sportx.sportx.util;
 
 
 import org.sportx.sportx.model.Ticket;
+import org.sportx.sportx.model.User;
 
 import java.sql.*;
 import java.time.LocalDate;
@@ -15,14 +16,15 @@ public class TicketDAO {
         this.conn = conn;
     }
 
-    public void inserir(Ticket ticket) throws SQLException {
-        String sql = "INSERT INTO tickets (submission_date, email, ticket_status, subject, message) VALUES (?, ?, ?, ?, ?)";
+    public void inserir(Ticket ticket, User user) throws SQLException {
+        String sql = "INSERT INTO ticket (submission_date, email, ticket_status, subject, message, user_id) VALUES (?, ?, ?, ?, ?, ?)";
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setDate(1, Date.valueOf(ticket.getSubmissionDate()));
             stmt.setString(2, ticket.getEmail());
             stmt.setString(3, ticket.getTicketStatus());
             stmt.setString(4, ticket.getSubject());
             stmt.setString(5, ticket.getMessage());
+            stmt.setInt(6, user.getUserId());
             stmt.executeUpdate();
         }
     }
